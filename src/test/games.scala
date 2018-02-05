dfb8779d98e8badf856638569ceff8f5144857b3
hyunjung <hyunii1227@gmail.com>
2018-02-05 18:52:14 +0900

    [현정] test.games.scala

---
+++ src/test/games.scala
@@ -0,0 +1,17 @@
+package test
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object games {
+  def main(args: Array[String]): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("InsightGameSpark"));
+    val game1 = sc.textFile("/data/predata/pre_gamenames.txt");
+    val game2 = sc.textFile("/data/predata/game_weights.txt");
+    val result = game1.union(game2)
+    val result2 = result.map(data => data.split("\n"))
+    val gameMap = result2.map(data => data(0).split(","))
+    val gameMap2 = gameMap.map(data => (data(0), data(1).toDouble))
+    // 이제 리듀서 만들기
+  }
+}
